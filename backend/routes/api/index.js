b55c84a84d8abf3d26bfa8310649feb4f93a7c71
Reyhaneh <Reyhaneh.Abdollahi2@gmail.com>
2022-08-12 12:03:58 -0400

    correction

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,28 +4,6 @@ const { User } = require('../../db/models');
 
 router.use(restoreUser);
 
-// router.get('/restore-user',(req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-// router.get('/require-auth',requireAuth,(req, res) => {
-//     return res.json(req.user);
-//   }
-// );
 
-// router.get('/set-token-cookie', async (_req, res) => {
-//   const user = await User.findOne({
-//       where: {
-//         email: 'demo@user.io'
-//       }
-//     });
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// });
-
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-//     // console.log({ requestBody: req.body })
-//   });
 
 module.exports = router;
