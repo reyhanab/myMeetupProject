4a6e194dbcfd522949b88544fdff8e509aac5e6b
Reyhaneh <Reyhaneh.Abdollahi2@gmail.com>
2023-01-27 21:37:18 -0500

    deploy

--- backend/db/seeders/20220812133610-demo-user.js
+++ backend/db/seeders/20220812133610-demo-user.js
@@ -1,45 +1,45 @@
 'use strict';
 const bcrypt = require('bcryptjs')
-
+const {User} = require('../models')
 let options = {validate : true, tablename : "users"};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object]
-
 }
+fields= [
+  {
+    email: 'user1@user.io',
+    firstName: 'Reyhaneh',
+    lastName: 'Abdollahi',
+    password: bcrypt.hashSync('password1')
+  },
+  {
+    email: 'user2@user.io',
+    firstName: 'John',
+    lastName: 'smith',
+    password: bcrypt.hashSync('password2')
+  },
+  {
+    email: 'user3@user.io',
+    firstName: 'Alice',
+    lastName: 'Smith',
+    password: bcrypt.hashSync('password3')
+  },
+  {
+    email: 'user4@user.io',
+    firstName: 'Don',
+    lastName: 'Hall',
+    password: bcrypt.hashSync('password4')
+  },
+  {
+    email: 'user5@user.io',
+    firstName: 'Steve',
+    lastName: 'Young',
+    password: bcrypt.hashSync('password5')
+  }
+]
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(options,[
-      {
-        email: 'user1@user.io',
-        firstName: 'Reyhaneh',
-        lastName: 'Abdollahi',
-        password: bcrypt.hashSync('password1')
-      },
-      {
-        email: 'user2@user.io',
-        firstName: 'John',
-        lastName: 'smith',
-        password: bcrypt.hashSync('password2')
-      },
-      {
-        email: 'user3@user.io',
-        firstName: 'Alice',
-        lastName: 'Smith',
-        password: bcrypt.hashSync('password3')
-      },
-      {
-        email: 'user4@user.io',
-        firstName: 'Don',
-        lastName: 'Hall',
-        password: bcrypt.hashSync('password4')
-      },
-      {
-        email: 'user5@user.io',
-        firstName: 'Steve',
-        lastName: 'Young',
-        password: bcrypt.hashSync('password5')
-      }
-    ])
+    await User.bulkCreate(fields)
   },
 
   async down (queryInterface, Sequelize) {
