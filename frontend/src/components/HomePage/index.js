6882a52f1afd73951665c10f0092af124d452809
Reyhaneh <Reyhaneh.Abdollahi2@gmail.com>
2023-02-13 11:17:54 -0500

    dev change

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -23,6 +23,18 @@ function Homepage(){
                         <NavLink className='name-group-preview' to={`/groups/${groupsArr[2]?.id}`}>{groupsArr[2]?.name}</NavLink>
                     </div>
                 </div>
+                <div className="events-container">
+                    <h3 className="your-events">Events</h3>
+                    <NavLink to='/events' className='see-all-events'>See all events</NavLink>
+                    <div className="events-prview">
+                        <img className="event-image-preview" src={eventsArr[0]?.previewImage} />
+                        <img className="event-image-preview" src={eventsArr[1]?.previewImage} />
+                        <img className="event-image-preview" src={eventsArr[2]?.previewImage} />
+                        <NavLink className='name-event-preview' to={`/events/${eventsArr[0]?.id}`}>{eventsArr[0]?.name}</NavLink>
+                        <NavLink className='name-event-preview' to={`/events/${eventsArr[1]?.id}`}>{eventsArr[1]?.name}</NavLink>
+                        <NavLink className='name-event-preview' to={`/events/${eventsArr[2]?.id}`}>{eventsArr[2]?.name}</NavLink>
+                    </div>
+                </div>
             </form>
 
         )
@@ -35,18 +47,6 @@ function Homepage(){
                         <div>Whatever you’re looking to do this year, Meetup can help. For 20 years, people have turned to Meetup to meet people, make friends, find support, grow a business, and explore their interests. Thousands of events are happening every day—join the fun.Whatever you’re looking to do this year, Meetup can help. For 20 years, people have turned to Meetup to meet people, make friends, find support, grow a business, and explore their interests. Thousands of events are happening every day—join the fun.</div>
                     </div>
                     <img className="homepage-image" src='https://secure.meetupstatic.com/next/images/shared/online_events.svg?w=640'></img>
-                </div>
-                <div className="events-container">
-                    <h3 className="your-events">Events</h3>
-                    <NavLink to='/events' className='see-all-events'>See all events</NavLink>
-                    <div className="events-prview">
-                        <img className="event-image-preview" src={eventsArr[0]?.previewImage} />
-                        <img className="event-image-preview" src={eventsArr[1]?.previewImage} />
-                        <img className="event-image-preview" src={eventsArr[2]?.previewImage} />
-                        <NavLink className='name-event-preview' to={`/events/${eventsArr[0]?.id}`}>{eventsArr[0]?.name}</NavLink>
-                        <NavLink className='name-event-preview' to={`/events/${eventsArr[1]?.id}`}>{eventsArr[1]?.name}</NavLink>
-                        <NavLink className='name-event-preview' to={`/events/${eventsArr[2]?.id}`}>{eventsArr[2]?.name}</NavLink>
-                    </div>
                 </div>
             </form>
         )
